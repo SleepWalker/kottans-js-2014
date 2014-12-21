a4f7d3a3401b765d0c865c37ecdc04f3aa6963ef
SleepWalker <mybox@udf.su>
2014-12-21 13:04:11 +0200

    Installed some dependencies for dev workflow

--- 10.1-nodejs-workshop/index.js
+++ 10.1-nodejs-workshop/index.js
@@ -1,1 +1,11 @@
-console.log('Hey, this is your app');
+var express = require('express'),
+    app = express(),
+    port = 8080;
+
+app.get('*', function(req, res) {
+    res.end('Hello World!');
+});
+
+app.listen(port);
+
+console.log('Server is up on port %s.', port);
